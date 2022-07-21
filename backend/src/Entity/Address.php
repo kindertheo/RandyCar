7e28a2fa55534f15dd9efbf9eb11d6d211023171
Théo <kinder.theo@gmail.com>
2022-07-21 14:42:30 +0200

    Modify property name : City

--- backend/src/Entity/Address.php
+++ backend/src/Entity/Address.php
@@ -35,7 +35,7 @@ class Address
      * @ORM\ManyToOne(targetEntity=City::class, inversedBy="addresses")
      * @ORM\JoinColumn(nullable=false)
      */
-    private $city_id;
+    private $city;
 
     /**
      * @ORM\OneToMany(targetEntity=Trip::class, mappedBy="start_address")
@@ -76,14 +76,14 @@ class Address
         return $this;
     }
 
-    public function getCityId(): ?City
+    public function getCity(): ?City
     {
-        return $this->city_id;
+        return $this->city;
     }
 
-    public function setCityId(?City $city_id): self
+    public function setCity(?City $city): self
     {
-        $this->city_id = $city_id;
+        $this->city = $city;
 
         return $this;
     }
