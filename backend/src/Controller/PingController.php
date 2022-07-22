10e0f00487e9419fef27b3dbda6d866f0ed910ed
Théo <kinder.theo@gmail.com>
2022-07-22 22:30:43 +0200

    rename class from pingcontroller

--- backend/src/Controller/PingController.php
+++ backend/src/Controller/PingController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class PingController extends AbstractController
 {
     /**
      * @Route("/ping", name="ping")
