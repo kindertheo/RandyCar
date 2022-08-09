41a41a7f5d88f0ce04f67d47e913a7dd972f74bb
martin <martin.aubertin@gmail.com>
2022-08-09 18:00:55 +0200

    created cartest.php

---
+++ backend/tests/CarTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Car;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class UserTest extends ApiTestCase
+{
+    protected function setUp(): void 
+    { 
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+    
+}
