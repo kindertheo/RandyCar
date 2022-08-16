20739537f7afb9d136c357fcfd9a3e6b5aaa437b
martin <martin.aubertin@gmail.com>
2022-08-16 16:53:12 +0200

    editing mail testing, wip

--- backend/tests/MailTest.php
+++ backend/tests/MailTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 use App\Entity\Mail;
+use App\Entity\User;
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 
 class MailTest extends ApiTestCase
@@ -50,6 +51,24 @@ class MailTest extends ApiTestCase
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
     }
 
+    public function getMail() 
+    {
+        $req = static::createClient()->request('GET', 'http://localhost/api/mails');
+        $queryResult = $this->entityManager 
+            ->getRepository(Mail::class)
+            ->count([]);
+
+        $content = $req->getContent();
+
+        $count = json_decode($content, true);
+        $count = $count['hydra:totalItems'];
+
+        $this->assertEquals($count, $queryResult);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
     //get{id}
     public function testGetById()
     {
@@ -66,6 +85,33 @@ class MailTest extends ApiTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    //post 
+    public function postMail()
+    {
+
+        $randomReceiver = $this->entityManager->getRepository(User::class)->findAll();
+        $randomReceiver = $randomReceiver[random_int(0, count($randomReceiver)-1 )];
+
+        $body = [ 
+            "receiver"=> $randomReceiver->getId(),
+            "object"=> "Lorem Ipsum Dolor sit amet",
+            "content"=> "This is a lorem ipsum content. If you see this that mean post testing is working correctly on this project",
+            "sentDate"=> "2022-08-16T08:22:46.806Z"
+        ];
+
+        $req = static::createClient()->request('POST', 'http://localhost/api/mails/', [ 
+            'headers' => [ 
+                'Content-Type' => 'application/json',
+                'accept' => 'application/json'
+            ],
+            'body' => json_encode($body)
+            ]
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/json; charset=utf-8');
     }
 
 }
