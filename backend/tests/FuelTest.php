3e64a2f4d2d42ceae83c95e44441b2200124d76e
martin <martin.aubertin@gmail.com>
2022-08-16 10:45:19 +0200

    adding fuel tests wip

---
+++ backend/tests/FuelTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Tests;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Fuel;
+use Doctrine\ORM\QueryBuilder;
+
+class FuelTest extends ApiTestCase
+{
+    private $entityManager;
+
+    protected function setUp(): void 
+    { 
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+    //GET
+    public function testGetFuel()
+    {
+        $req = static::createClient()->request('GET', 'http://localhost/api/fuel');
+        $queryResult = $this->entityManager 
+            ->getRepository(Fuel::class)
+            ->count([]);
+
+        $content = $req->getContent();
+
+        $count = json_decode($content, true);
+        $count = $count['hydra:totalItems'];
+
+        $this->assertEquals($count, $queryResult);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    public function testJsonFormat(): void 
+    { 
+        $response = static::createClient()->request('GET', 'http://localhost/api/fuel');
+
+        $this->assertMatchesResourceCollectionJsonSchema(Fuel::class);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    //get{id}
+    public function testGetById()
+    {
+        // findAll
+        $id = $this->entityManager->getRepository(Fuel::class)->findAll();
+        $objectId = $id[0];
+        $this->assertIsObject($objectId); 
+
+        //get Object + index separate
+        $index = $objectId->getId(); 
+        $this->assertIsNumeric($index);
+
+        // use Index as slug
+        $response = static::createClient()->request('GET', 'http://localhost/api/fuel/'. $index);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+
+
+}
