0cb1a8638be2054ac1354b5364c71251a2b994d0
Théo <kinder.theo@gmail.com>
2022-08-21 14:50:12 +0200

    add user verification to entity and tests

--- backend/tests/FuelTest.php
+++ backend/tests/FuelTest.php
@@ -14,6 +14,9 @@ class FuelTest extends ApiTestCase
 {
     private $entityManager;
 
+    private $tokenAdmin;
+    private $tokenUser;
+
     protected function setUp(): void 
     { 
         $kernel = self::bootKernel();
@@ -21,20 +24,71 @@ class FuelTest extends ApiTestCase
         $this->entityManager = $kernel->getContainer()
             ->get('doctrine')
             ->getManager();
+
+        $this->admin = Utils::createUser(True);
+        $this->user = Utils::createUser(False);
+
+        $this->tokenAdmin = Utils::getToken($this->admin);
+        $this->tokenUser = Utils::getToken($this->user);
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
 
+        Utils::deleteUser($this->admin, $this->entityManager);
+        Utils::deleteUser($this->user, $this->entityManager);
+
         $this->entityManager->close();
         $this->entityManager = null;
     }
 
+    private function requestFuel(string $method, string $url, int $randomIdCar, string $token = Null){
+        $headers = [ 
+            'Content-Type' => 'application/json',
+            'accept' => 'application/json',
+        ];
+        if($token){
+            $headers['Authorization'] = 'bearer ' . $token;
+        }
+
+        $req = static::createClient()->request($method, $url, [ 
+            'headers' => $headers,
+            'body' => json_encode([
+                'name' => "TestPut",
+                'consumption'=> 6.66,
+                'cars' => ["api/cars/". $randomIdCar],
+            ])
+        ]);
+
+        return $req;
+    }
+
+
     //GET
-    public function testGetFuel()
+    public function testGetFuelUser()
     {
-        $req = static::createClient()->request('GET', 'http://localhost/api/fuels');
+        //test with Auth Admin
+        $req = static::createClient()->request('GET', 'http://localhost/api/fuels', ['auth_bearer' => $this->tokenUser]);
+        $queryResult = $this->entityManager 
+            ->getRepository(Fuel::class)
+            ->count([]);
+
+        $content = $req->getContent();
+        $count = json_decode($content, true);
+        $count = $count['hydra:totalItems'];
+
+        $this->assertEquals($count, $queryResult);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    //GET
+    public function testGetFuelAdmin()
+    {
+        //test with Auth Admin
+        $req = static::createClient()->request('GET', 'http://localhost/api/fuels', ['auth_bearer' => $this->tokenAdmin]);
         $queryResult = $this->entityManager 
             ->getRepository(Fuel::class)
             ->count([]);
@@ -52,7 +106,7 @@ class FuelTest extends ApiTestCase
 
     public function testJsonFormat(): void 
     { 
-        $response = static::createClient()->request('GET', 'http://localhost/api/fuels');
+        $response = static::createClient()->request('GET', 'http://localhost/api/fuels', ['auth_bearer' => $this->tokenUser] );
 
         $this->assertMatchesResourceCollectionJsonSchema(Fuel::class);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
@@ -71,7 +125,7 @@ class FuelTest extends ApiTestCase
         $this->assertIsNumeric($index);
 
         // use Index as slug
-        $response = static::createClient()->request('GET', 'http://localhost/api/fuels/'. $index);
+        $response = static::createClient()->request('GET', 'http://localhost/api/fuels/'. $index, ['auth_bearer' => $this->tokenUser]);
 
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
@@ -83,21 +137,16 @@ class FuelTest extends ApiTestCase
      { 
         $randomIdCar = Utils::getRandomIdByCollections(Car::class, $this->entityManager);
 
-        // $allIdCar = $this->entityManager->getRepository(Car::class)->findAll();
-        // $allIdCar = count($allIdCar);
-        // $randomIdCar = random_int(1, 10);
-        $req = static::createClient()->request('POST','http://localhost/api/fuels', [
-            'headers' => [ 
-                'Content-Type' => 'application/json',
-                'accept' => 'application/json'
-            ],
-            'body' => json_encode([
-                'name' => "Test",
-                'consumption'=> 1.56,
-                'cars' => ["api/cars/". $randomIdCar],
-            ])
-            ]
-        );        
+        //Without Token => Expected error : Token not found
+        $req = $this->requestFuel('POST', 'http://localhost/api/fuels', $randomIdCar);
+        $this->assertResponseStatusCodeSame(401);
+
+        //With User token => Access Denied
+        $req = $this->requestFuel('POST', 'http://localhost/api/fuels', $randomIdCar, $this->tokenUser);
+        $this->assertResponseStatusCodeSame(403);
+
+        //With Admin Token => Success
+        $req = $this->requestFuel('POST', 'http://localhost/api/fuels', $randomIdCar, $this->tokenAdmin);
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('content-type', 'application/json; charset=utf-8');
      }
@@ -107,29 +156,35 @@ class FuelTest extends ApiTestCase
      { 
         $randomIdFuel = Utils::getRandomIdByCollections(Fuel::class, $this->entityManager);
 
+        //No Token Found
         $req = static::createClient()->request('DELETE', 'http://localhost/api/fuels/' . $randomIdFuel);
-        $this->assertResponseStatusCodeSame(204);
+        $this->assertResponseStatusCodeSame(401);
+
+        //Access Denied
+        $req = static::createClient()->request('DELETE', 'http://localhost/api/fuels/' . $randomIdFuel, ['auth_bearer' => $this->tokenUser]);
+        $this->assertResponseStatusCodeSame(403);
+
+        //Auth
+        $req = static::createClient()->request('DELETE', 'http://localhost/api/fuels/' . $randomIdFuel, ['auth_bearer' => $this->tokenAdmin]);
+        $this->assertResponseStatusCodeSame(204);        
     }
 
- 
      // TEST PUT 
      public function testPutFuel() 
      { 
         $randomIdCar = Utils::getRandomIdByCollections(Car::class, $this->entityManager);
         $randomIdFuel = Utils::getRandomIdByCollections(Fuel::class, $this->entityManager);
  
-         $req = static::createClient()->request('PUT', 'http://localhost/api/fuels/' . $randomIdFuel, [ 
-             'headers' => [ 
-             'Content-Type' => 'application/json',
-             'accept' => 'application/json'
-         ],
-         'body' => json_encode([
-            'name' => "TestPut",
-            'consumption'=> 6.66,
-            'cars' => ["api/cars/". $randomIdCar],
-         ])
-         ] );
- 
+        //Token not found
+        $req = $this->requestFuel('PUT', 'http://localhost/api/fuels/' . $randomIdFuel, $randomIdCar);
+        $this->assertResponseStatusCodeSame(401);
+
+        //Access Denied
+        $req = $this->requestFuel('PUT', 'http://localhost/api/fuels/' . $randomIdFuel, $randomIdCar, $this->tokenUser);
+        $this->assertResponseStatusCodeSame(403);
+
+        //Is Auth
+         $req =  $this->requestFuel('PUT', 'http://localhost/api/fuels/' . $randomIdFuel, $randomIdCar, $this->tokenAdmin);
          $this->assertResponseIsSuccessful();
      }
 }
