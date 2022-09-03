59ad2d70349d41d9ff6a70417a742e3f250ef3d2
Théo <kinder.theo@gmail.com>
2022-09-03 17:19:09 +0200

    add footer and header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LandscapeComponent } from './component/landscape/landscape.component';
+import { HomeComponent } from './component/home/home.component';
+import { HomeCorpusComponent } from './component/home-corpus/home-corpus.component';
+import { HomeHeadlineComponent } from './component/home-headline/home-headline.component';
+import { HomeGraphComponent } from './component/home-graph/home-graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    LandscapeComponent,
+    HomeComponent,
+    HomeCorpusComponent,
+    HomeHeadlineComponent,
+    HomeGraphComponent
   ],
   imports: [
     BrowserModule,
