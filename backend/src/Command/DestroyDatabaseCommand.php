07480571b9e616859fd0e8855ffa894cbc0b5561
Théo <kinder.theo@gmail.com>
2022-09-06 23:45:24 +0200

    update command, add quiet arg

--- backend/src/Command/DestroyDatabaseCommand.php
+++ backend/src/Command/DestroyDatabaseCommand.php
@@ -7,73 +7,79 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class DestroyDatabaseCommand extends Command
 {
     // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'destroy:database:test';
+    protected static $defaultName = 'destroy:database';
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('env', InputArgument::OPTIONAL, 'Which environment you want to destroy?')
+            ->addArgument('quiet', null, InputOption::VALUE_NONE);
+
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        
-        // outputs a message followed by a "\n"
-        $output->writeln('Whoa!');
-        $output->writeln('You are about to DESTROY THE DATABAAAAAAASE');
-        
+        try{
 
-        $command = $this->getApplication()->find('doctrine:database:drop');
+            $env = $input->getArgument('env');
+            switch($env){
+                case 'dev':
+                    break;
+                case 'test':
+                    break;
+                default:
+                    $env = 'dev';
+                    $output->writeln('No env specified, will use dev environment!');
+                    $output->writeln('Next try use php bin/console destroy:database [<env>]');
 
-        $arguments = [
-            '--force' => true,
-            '--env' => 'test'
-        ];
+            }
+            if($input->getArgument('quiet')){
+                $output->setVerbosity(OutputInterface::VERBOSITY_QUIET);
+            }
 
-        $greetInput = new ArrayInput($arguments);
-        $returnCode = $command->run($greetInput, $output);
+            // outputs a message followed by a "\n"
+            $output->writeln('Whoa!');
+            $output->writeln('You are about to DESTROY THE DATABAAAAAAASE');
+            
+            $command = $this->getApplication()->find('doctrine:database:drop');
+            $arguments = new ArrayInput([
+                '--force' => true,
+                '--no-interaction' => true,
+                '--env' => $env,
+            ]);
+            
+            $arguments->setInteractive(false);
+            $returnCode = $command->run($arguments, $output);
+    
+            $arguments = new ArrayInput([
+                '--no-interaction' => true,
+                '--env' => $env,
+            ]);
+            
+            $arguments->setInteractive(false);
+            $command = $this->getApplication()->find('doctrine:database:create');
+            $returnCode = $command->run($arguments, $output);
 
+            $command = $this->getApplication()->find('doctrine:schema:create');
+            $returnCode = $command->run($arguments, $output);
 
+            $command = $this->getApplication()->find('doctrine:fixtures:load');
+            $returnCode = $command->run($arguments, $output);
 
-        $command = $this->getApplication()->find('doctrine:database:create');
-        $arguments = [
-            '--env' => 'test'
-        ];
-        $greetInput = new ArrayInput($arguments);
-        $returnCode = $command->run($greetInput, $output);
+            $output->setVerbosity(OutputInterface::VERBOSITY_NORMAL);
+            $output->writeln('Database successfully recreated');
 
+            return Command::SUCCESS;
 
-
-        $command = $this->getApplication()->find('doctrine:schema:create');
-        $arguments = [
-            '--no-interaction' => true,
-            '--env' => 'test'
-        ];
-        $greetInput = new ArrayInput($arguments);
-        $returnCode = $command->run($greetInput, $output);
-
-
-        $arguments = new ArrayInput([
-            '--env' => 'test'
-        ]);
-        $arguments->setInteractive(false);
-        
-        $command = $this->getApplication()->find('doctrine:fixtures:load');
-        $returnCode = $command->run($arguments, $output);
-
-        // ... put here the code to create the user
-
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
-
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
-        return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
+        } 
+        catch (Exception $e){
+            return Command::FAILURE;
+        }
     }
 }
